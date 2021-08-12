474e5f1bdc0ec3e03c20f1303ada3b4d3f0b0c8d
Octavio Ponce <Tavo0402>
2021-08-12 00:22:58 -0600

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        //[HttpGet]
+        //public string GetProducts()
+        //{
+        //    return "This will be a list of products";
+        //}
+
+        //[HttpGet]
+        //public ActionResult<List<Product>> GetProducts()
+        //{
+        //    var products = _context.Products.ToList();
+        //    return products;
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrands(int id)
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<ProductType>> GetProductTypes(int id)
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,83 @@
+﻿using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var brand in brands)
+                    {
+                        context.ProductBrands.Add(new ProductBrand()
+                        {
+                            Name = brand.Name
+                        });
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    var typesData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                    foreach (var type in types)
+                    {
+                        context.ProductTypes.Add(new ProductType()
+                        {
+                            Name = type.Name
+                        });
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    var productsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    foreach (var product in products)
+                    {
+                        context.Products.Add(new Product() { 
+                            Name = product.Name,
+                            Description = product.Description,
+                            Price = product.Price,
+                            PictureUrl = product.PictureUrl,
+                            ProductTypeId = product.ProductTypeId,
+                            ProductBrandId = product.ProductBrandId
+                        });
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
